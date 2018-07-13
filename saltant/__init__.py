b254edb369227f515b7a94886448b4d338956ee0
Matt Wiens <mwiens91@gmail.com>
2018-07-13 03:34:46 +0000

    Fix environment variable WSGI issue
    Basically, when using WSGI, the process tries to access environment
    variables in an __init__.py of the saltant directory before these
    environment variables are loaded in the wsgi.py module.

--- saltant/__init__.py
+++ saltant/__init__.py
@@ -1,5 +1,17 @@
-"""Make sure shared_tasks use the celery app."""
+"""Things here get run first.
 
+This initializes the Celery app, and makes sure that environment
+variables are loaded for it.
+"""
+
+import os
+import dotenv
+
+# Load environment variables
+dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
+
+# Initialize Celery (see
+# http://docs.celeryproject.org/en/latest/django/first-steps-with-django.html)
 from saltant.celery import app as celery_app
 
 __all__ = ('celery_app',)
