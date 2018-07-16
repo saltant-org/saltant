2e6b303b5a7c3a9dabd71da21534dc0c2a89a9e9
Matt Wiens <mwiens91@gmail.com>
2018-07-16 23:42:04 +0000

    Allow workers to run with less dependencies

--- saltant/celery.py
+++ saltant/celery.py
@@ -3,24 +3,14 @@
 import os
 from celery import Celery
 from saltant import settings
+from tasksapi.tasks import *
 
-# Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'saltant.settings')
 
+# Run the celery app
 app = Celery('saltant')
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings', namespace='CELERY')
-
-# For some reason the BROKER_USE_SSL isn't being loaded properly by the
-# above command
-try:
-    app.conf.update(broker_use_ssl=settings.BROKER_USE_SSL)
-except AttributeError:
-    pass
-
-# Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+# Set the config options specified in settings
+app.conf.update(
+    broker_url=settings.CELERY_BROKER_URL,
+    broker_use_ssl=settings.BROKER_USE_SSL,
+    timezone=settings.CELERY_TIMEZONE,)
