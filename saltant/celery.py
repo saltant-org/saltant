744709e392b06c507b025d06c008b00b9c32f9e1
Matt Wiens <mwiens91@gmail.com>
2018-07-16 16:57:51 -0700

    Actually hotfix 2e6b303b5a7c3a9dabd71da21534dc0c2a89a9e9

--- saltant/celery.py
+++ saltant/celery.py
@@ -12,7 +12,6 @@ app = Celery('saltant')
 # Set the config options specified in settings
 app.conf.update(
     broker_url=settings.CELERY_BROKER_URL,
-    broker_use_ssl=settings.BROKER_USE_SSL,
     timezone=settings.CELERY_TIMEZONE,)
 
 # Set SSL setting if we're using SSL
