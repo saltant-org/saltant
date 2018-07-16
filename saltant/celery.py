6236aee86910e17d362cebd7b73baec9f38ea97f
Matt Wiens <mwiens91@gmail.com>
2018-07-16 16:52:30 -0700

    Hotfix 2e6b303b5a7c3a9dabd71da21534dc0c2a89a9e9

--- saltant/celery.py
+++ saltant/celery.py
@@ -14,3 +14,9 @@ app.conf.update(
     broker_url=settings.CELERY_BROKER_URL,
     broker_use_ssl=settings.BROKER_USE_SSL,
     timezone=settings.CELERY_TIMEZONE,)
+
+# Set SSL setting if we're using SSL
+try:
+    app.conf.update(broker_use_ssl=settings.BROKER_USE_SSL)
+except AttributeError:
+    pass
