ffe7d61ddaa21025c0f51b420742968272d11fe9
Matt Wiens <mwiens91@gmail.com>
2018-07-21 22:26:38 -0700

    Delete all instances created in models test

--- tasksapi/tests.py
+++ tasksapi/tests.py
@@ -22,9 +22,12 @@ class TasksApiModelTests(TestCase):
     """Test that we can instantiate models correctly."""
     def test_model_creation(self):
         """Make sure basic model instantiation doesn't fail."""
+        # Create a user
         user = User.objects.create(
             username='AzureDiamond',
             password='hunter2',)
+
+        # Create a task type
         tasktype = TaskType.objects.create(
             name='my-task-type',
             description="Fantastic task type",
@@ -37,15 +40,26 @@ class TasksApiModelTests(TestCase):
             required_arguments=['name'],
             required_arguments_default_values={'name': 'AzureDiamond'},
             directories_to_bind={},)
+
+        # Create a task queue
         taskqueue = TaskQueue.objects.create(
             name='my-task-queue',
             description="Fantastic task queue",
             user=user,
             private=False,
             active=True,)
+
+        # Create a task instance of the task type we made and assign it
+        # to the task queue we made
         taskinstance = TaskInstance.objects.create(
             name='my-task-instance',
             user=user,
             task_type=tasktype,
             task_queue=taskqueue,
             arguments={'name': 'Daniel'},)
+
+        # Now delete everything
+        taskinstance.delete()
+        taskqueue.delete()
+        tasktype.delete()
+        user.delete()
