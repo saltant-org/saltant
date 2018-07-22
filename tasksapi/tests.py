868058dc0f9be8eade7b24904b6b25e64268986a
Matt Wiens <mwiens91@gmail.com>
2018-07-21 18:36:32 -0700

    Add tasksapi model instantiation tests

--- tasksapi/tests.py
+++ tasksapi/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+"""Contains tasks for the tasksapi.
 
-# Create your tests here.
+This assumes a few things:
+ - you're using the regular user model from django.contrib.auth
+ - you've enabled (built-in) DRF token authentication
+ - you've enabled DRF JWT authentication
+"""
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+from rest_framework.authtoken.models import Token
+from tasksapi.constants import (
+    DOCKER,
+    SINGULARITY,)
+from tasksapi.models import (
+    TaskType,
+    TaskQueue,
+    TaskInstance,)
+
+
+class TasksApiModelTests(TestCase):
+    """Test that we can instantiate models correctly."""
+    def test_model_creation(self):
+        """Make sure basic model instantiation doesn't fail."""
+        user = User.objects.create(
+            username='AzureDiamond',
+            password='hunter2',)
+        tasktype = TaskType.objects.create(
+            name='my-task-type',
+            description="Fantastic task type",
+            user=user,
+            container_image='mwiens91/hello-world',
+            container_type=DOCKER,
+            script_path='apps/hello_world.py',
+            logs_path='/logs/',
+            environment_variables=['HOME'],
+            required_arguments=['name'],
+            required_arguments_default_values={'name': 'AzureDiamond'},
+            directories_to_bind={},)
+        taskqueue = TaskQueue.objects.create(
+            name='my-task-queue',
+            description="Fantastic task queue",
+            user=user,
+            private=False,
+            active=True,)
+        taskinstance = TaskInstance.objects.create(
+            name='my-task-instance',
+            user=user,
+            task_type=tasktype,
+            task_queue=taskqueue,
+            arguments={'name': 'Daniel'},)
