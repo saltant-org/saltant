692f8db37aa4bd28bc82a62b876801ee1218a5e1
Matt Wiens <mwiens91@gmail.com>
2018-08-23 15:23:48 -0700

    Trim a few __init__ package imports

--- tasksapi/models/__init__.py
+++ tasksapi/models/__init__.py
@@ -5,7 +5,5 @@ from .abstract_tasks import (
     AbstractTaskType,)
 from .container_tasks import (
     ContainerTaskInstance,
-    ContainerTaskType,
-    container_task_instance_pre_save_handler,
-    container_task_instance_post_save_handler,)
+    ContainerTaskType,)
 from .task_queues import TaskQueue
--- tasksapi/tasks/__init__.py
+++ tasksapi/tasks/__init__.py
@@ -1,12 +1,7 @@
 """Collect import task code to "export" from this directory."""
 
 from .base_task import (
-    run_task,
-    task_sent_handler,
-    task_prerun_handler,
-    task_success_handler,
-    task_failure_handler,
-    task_revoked_handler,)
+    run_task,)
 from .container_tasks import (
     run_docker_container_command,
     run_singularity_container_command,)
