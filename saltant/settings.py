0290c7d999b3a9edb3caab5c3b554fb50993edd3
Matt Wiens <mwiens91@gmail.com>
2018-09-03 16:32:04 -0700

    Add ability to set default time zone

--- saltant/settings.py
+++ saltant/settings.py
@@ -173,7 +173,7 @@ if not IM_A_CELERY_WORKER:
     # Internationalization
     # https://docs.djangoproject.com/en/2.0/topics/i18n/
     LANGUAGE_CODE = 'en-us'
-    TIME_ZONE = 'UTC'
+    TIME_ZONE = os.environ['TIME_ZONE']
     USE_I18N = True
     USE_L10N = True
     USE_TZ = True
