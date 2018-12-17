f86174849e733f4a7893f87d9661952617465ab5
Matt Wiens <mwiens91@gmail.com>
2018-12-17 15:59:05 -0800

    Fix URLname typos in 3af6bf8a8089d0d0ff05e4c6ff248eebbee3595c

--- frontend/tests.py
+++ frontend/tests.py
@@ -130,8 +130,8 @@ class FrontendRenderTests(TestCase):
             reverse("queue-detail", kwargs={"pk": TASK_QUEUE_PK}),
             reverse("queue-delete", kwargs={"pk": TASK_QUEUE_PK}),
             reverse("queue-update", kwargs={"pk": TASK_QUEUE_PK}),
-            reverse("taskwhitelists-list"),
-            reverse("taskwhitelists-create"),
+            reverse("taskwhitelist-list"),
+            reverse("taskwhitelist-create"),
         ]
 
         # Iterate over each page and make sure it's okay
